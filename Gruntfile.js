3f23729acce29bc3fd7cd7044dc407914fd1f2d4
hiddenwaffle <hiddenwaffle@users.noreply.github.com>
2015-12-09 19:04:36 -0600

    Remove extranous whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,6 +47,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-bowercopy');
     grunt.loadNpmTasks('grunt-contrib-connect');
-    
+
     grunt.registerTask('default', ['browserify', 'bowercopy', 'connect', 'watch']);
 };
